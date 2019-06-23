7be712478c12ef45080f5b7ddb61d8f647f3026a
Santosh Kumar <a.santoshkumar1995@gmail.com>
2019-06-22 17:21:04 -0700

    Create 1003. Check If Word Is Valid After Substitutions.py

---
+++ 1003. Check If Word Is Valid After Substitutions.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isValid(self, S: str) -> bool:
+        while("abc" in S):
+            S = S.replace("abc","")
+        if(len(S) == 0):
+            return True
+        return False
