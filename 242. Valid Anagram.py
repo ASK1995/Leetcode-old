8f4ce80f3225f67c0ca2f863f1c7822518e665a7
Santosh Kumar <a.santoshkumar1995@gmail.com>
2019-07-10 20:07:47 -0700

    Update 242. Valid Anagram.py

--- 242. Valid Anagram.py
+++ 242. Valid Anagram.py
@@ -1,13 +1,24 @@
 from collections import defaultdict
 
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
         d = defaultdict(lambda:0)
-        for i in s:
-            d[i] += 1
-        for i in t:
-            if(d[i] > 0):
-                d[i] -= 1
-            else:
+        
+        for letter in s:
+            d[letter] += 1
+        
+        for letter in t:
+            if(d[letter] == 0):
                 return False
+            d[letter] -= 1
+        
+        for key, value in d.items():
+            if(value > 0):
+                return False
+            
         return (all(values == 0 for values in d.values()) )
