3f36cf6c786ce203f32d0bed6c465bf2add0ccf9
Santosh Kumar <a.santoshkumar1995@gmail.com>
2019-07-10 18:03:06 -0700

    Update 20. Valid Parentheses.py

--- 20. Valid Parentheses.py
+++ 20. Valid Parentheses.py
@@ -1,22 +1,29 @@
-class Solution:
-    def isValid(self, s: str) -> bool:
-        starters = set(["(", "[", "{"])
-        enders = set([")", "}", "]"])
-        stack_s = []
-        for i in s:
-            if(i in starters):
-                stack_s.append(i)
-            elif(i in enders and len(stack_s) != 0):
-                if(stack_s[-1] == "(" and i == ')' ):
-                    stack_s.pop()
-                elif(stack_s[-1] == "[" and i == ']' ):
-                    stack_s.pop()
-                elif(stack_s[-1] == "{" and i == '}' ):
-                    stack_s.pop()
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        current = []
+        openers = set(['(', '[', '{'])
+        closers = set([')', ']', '}'])
+        
+        for bracket in s:
+            if(bracket in openers):
+                current.append(bracket)
+            else:
+                if(len(current) == 0):
+                    return False
+                elif(bracket == "}" and current[-1] == "{"):
+                    current.pop()
+                elif(bracket == "]" and current[-1] == "["):
+                    current.pop()
+                elif(bracket == ")" and current[-1] == "("):
+                    current.pop()
                 else:
                     return False
-            else:
-                return False
-        if(len(stack_s) == 0):
+                
+        if(len(current) == 0):
             return True
         return False
+            
