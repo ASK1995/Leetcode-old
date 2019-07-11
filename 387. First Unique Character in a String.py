59a65734916872fbc3ed46eb76f4f3889402855d
Santosh Kumar <a.santoshkumar1995@gmail.com>
2019-07-10 22:13:00 -0700

    Update 387. First Unique Character in a String.py

--- 387. First Unique Character in a String.py
+++ 387. First Unique Character in a String.py
@@ -1,11 +1,18 @@
 from collections import defaultdict
 
-class Solution:
-    def firstUniqChar(self, s: str) -> int:
-        d = defaultdict(lambda : 0)
-        for i in s:
-            d[i] += 1
-        for index, value in enumerate(s):
+class Solution(object):
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        d = defaultdict(lambda:0)
+        
+        for letter in s:
+            d[letter] += 1
+        
+        for index,value in enumerate(s):
             if(d[value] == 1):
                 return index
+        
         return -1
